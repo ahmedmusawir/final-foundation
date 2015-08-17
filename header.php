56e3c753f1e098d3ec0f310546070627475ee10e
Ahmed Musawir <ahmed.musawir@gmail.com>
2015-08-17 23:19:22 +0800

    2nd Commit: Blog Page Fin

--- header.php
+++ header.php
@@ -4,35 +4,43 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Final Foundation</title>
+    <link rel="stylesheet" href="scss/bower_components/font-awesome/css/font-awesome.min.css" />
     <link rel="stylesheet" href="style.css" />
     <script src="bower_components/modernizr/modernizr.js"></script>
   </head>
   <body>
-  <nav class="top-bar" data-topbar role="navigation">
-  <ul class="title-area">
-    <li class="name">
-      <h1><a href="#">My Site</a></h1>
-    </li>
-     <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-  </ul>
+  <div class="contain-to-grid">
+    <nav class="top-bar" data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+          <h1><a href="#">My Site</a></h1>
+        </li>
+         <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+        <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+      </ul>
 
-  <section class="top-bar-section">
-    <!-- Right Nav Section -->
-    <ul class="right">
-      <li class="active"><a href="#">Right Button Active</a></li>
-      <li class="has-dropdown">
-        <a href="#">Right Button Dropdown</a>
-        <ul class="dropdown">
-          <li><a href="#">First link in dropdown</a></li>
-          <li class="active"><a href="#">Active link in dropdown</a></li>
+      <section class="top-bar-section">
+        <!-- Right Nav Section -->
+        <ul class="right">
+          <li class="active"><a href="index.php"><i class="fa fa-lg fa-home"></i> Home</a></li>
+          <li class=""><a href="about.php"><i class="fa fa-lg fa-firefox"></i> About</a></li>
+          <li class=""><a href="portfolio.php"><i class="fa fa-lg fa-gg"></i> Portfolio</a></li>
+          <li class=""><a href="blog.php"><i class="fa fa-lg fa-group"></i>  Blog</a></li>
+          <li class=""><a href="contact.php"><i class="fa fa-lg fa-mobile"></i> Contact</a></li>
+          <li class="has-dropdown">
+            <a href="#">Dropdown</a>
+
+            <ul class="dropdown">
+              <li><a href="#">Dropdown 1</a></li>
+              <li class="active"><a href="#">Active link in dropdown</a></li>
+            </ul>
+          </li>
         </ul>
-      </li>
-    </ul>
 
-    <!-- Left Nav Section -->
-    <ul class="left">
-      <li><a href="#">Left Nav Button</a></li>
-    </ul>
-  </section>
-</nav>
+        <!-- Left Nav Section -->
+        <ul class="left">
+          <li><a href="#">Left Nav Button</a></li>
+        </ul>
+      </section>
+    </nav>
+  </div>
