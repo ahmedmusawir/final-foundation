2602bca56f1dd135bc7b8046ce4ccb4c52cb2d97
Ahmed Musawir <ahmed.musawir@gmail.com>
2015-08-19 19:03:29 +0800

    6th Commit: Frontpage Block 3 on going ...

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
- <?php include "header.php" ?>
+<?php include "header.php" ?>
+
+<style type="text/css" media="screen">
+
+  header.frontpage-header {
+      background: url(images/frontpage-galaxy-4.jpg) no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+
+</style>
  
 <header id="header" class="frontpage-header">
 
@@ -15,9 +27,91 @@
 </header><!-- /header -->
 
 <section class="main-content">
-  <div class="frontpage-background-block">
+
+  <div class="frontpage-background-block-1">
     
     <div class="row content-row">
+    
+          <article class="frontpage-block-1">
+
+            <div class="large-5 medium-5 columns">
+
+              <div class="flex-video widescreen vimeo">
+                <!-- <iframe src="http://player.vimeo.com/video/136114837" width="400" height="225" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> -->
+                <iframe src="//player.vimeo.com/video/39822385?title=0&amp;byline=0&amp;portrait=0" style="width: 100%; height: auto; border: 0;" allowfullscreen></iframe>
+              </div>
+
+            </div>
+            <div class="frontpage-content-1 large-7 medium-7 columns">
+              
+              <h3><a href="#" title="">Article Title</a></h3>
+              
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. 
+              </p>
+            </div>
+            <!-- <a href="#" class="button right" title="">Read More</a> -->
+
+          </article>
+
+        
+    </div> <!-- CONTENT ROW -->
+
+  </div> <!-- FRONTPAGE-BACKGROUND-BLOCK-1 -->
+
+  <div class="frontpage-background-block-2">
+    
+    <div class="row content-row">
+    
+          <article class="frontpage-block-2 clearfix">
+
+
+            <div class="frontpage-content-2 large-7 medium-7 columns">
+              <h3><a href="#" title="">Article Title</a></h3>
+              <p>
+                <small>
+                  Posted on <em><a href="#" title="">2 June 2015</a></em> by <a href="#" title="">Da Moose</a> in
+                  <a href="#" title="">Cars</a>, <a href="#" title="">Sports Cars</a>
+                </small></p>
+              <p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. 
+              </p>
+              <a href="#" class="button right" title="">Read More</a>
+
+            </div>
+
+             <div class="large-5 medium-5 columns">
+
+              <div class="flex-video widescreen vimeo">
+                <!-- <iframe src="http://player.vimeo.com/video/136114837" width="400" height="225" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> -->
+                <iframe src="//player.vimeo.com/video/35565030?title=0&amp;byline=0&amp;portrait=0" style="width: 100%; height: auto; border: 0;" allowfullscreen></iframe>
+              </div>
+              <div class="flex-video widescreen vimeo">
+                <!-- <iframe src="http://player.vimeo.com/video/136114837" width="400" height="225" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> -->
+                <iframe src="//player.vimeo.com/video/113142476?title=0&amp;byline=0&amp;portrait=0" style="width: 100%; height: auto; border: 0;" allowfullscreen></iframe>
+              </div>
+
+            </div>
+
+          </article>
+
+        
+    </div> <!-- CONTENT ROW -->
+
+  </div> <!-- FRONTPAGE-BACKGROUND-BLOCK-2 -->
+
+  <div class="frontpage-background-block-3">
+    
+      <div class="row content-row">
     
           <article class="frontpage-block clearfix">
 
@@ -46,9 +140,79 @@
           </article>
 
         
+      </div> <!-- CONTENT ROW -->
+
+  </div> <!-- FRONTPAGE-BACKGROUND-BLOCK-3 -->
+
+  <div class="frontpage-background-block-2">
+    
+    <div class="row content-row">
+    
+          <article class="frontpage-block clearfix">
+
+            <div class="large-5 medium-5 columns">
+              <!-- <a class="" href="#" title=""><img class="img-responsive" src="images/blog-img-1.jpeg" alt=""></a> -->
+              <!-- <a class="" href="#" title=""><img class="img-responsive" src="http://lorempixel.com/640/640/abstract/6" alt=""></a> -->
+            </div>
+            <div class="frontpage-content large-7 medium-7 columns">
+              <h3><a href="#" title="">Article Title</a></h3>
+              <p>
+                <small>
+                  Posted on <em><a href="#" title="">2 June 2015</a></em> by <a href="#" title="">Da Moose</a> in
+                  <a href="#" title="">Cars</a>, <a href="#" title="">Sports Cars</a>
+                </small></p>
+              <p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. 
+              </p>
+            </div>
+            <a href="#" class="button right" title="">Read More</a>
+
+          </article>
+
+        
     </div> <!-- CONTENT ROW -->
 
-  </div> <!-- FRONTPAGE-BACKGROUND-BLOCK -->
+  </div> <!-- FRONTPAGE-BACKGROUND-BLOCK-2 -->
+
+  <div class="frontpage-background-block-1">
+    
+    <div class="row content-row">
+    
+          <article class="frontpage-block clearfix">
+
+            <div class="large-5 medium-5 columns">
+              <!-- <a class="" href="#" title=""><img class="img-responsive" src="images/blog-img-1.jpeg" alt=""></a> -->
+              <!-- <a class="" href="#" title=""><img class="img-responsive" src="http://lorempixel.com/640/640/abstract/6" alt=""></a> -->
+            </div>
+            <div class="frontpage-content large-7 medium-7 columns">
+              <h3><a href="#" title="">Article Title</a></h3>
+              <p>
+                <small>
+                  Posted on <em><a href="#" title="">2 June 2015</a></em> by <a href="#" title="">Da Moose</a> in
+                  <a href="#" title="">Cars</a>, <a href="#" title="">Sports Cars</a>
+                </small></p>
+              <p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. 
+              </p>
+            </div>
+            <a href="#" class="button right" title="">Read More</a>
+
+          </article>
+
+        
+    </div> <!-- CONTENT ROW -->
+
+  </div> <!-- FRONTPAGE-BACKGROUND-BLOCK-1 -->
 
 </section> <!-- MAIN CONTENT -->
     
