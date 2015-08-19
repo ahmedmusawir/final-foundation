904d69f27229acb045093c67965a7aff539a57cb
Ahmed Musawir <ahmed.musawir@gmail.com>
2015-08-19 22:34:11 +0800

    7th Commit: Frontpage Block4 on going

--- index.php
+++ index.php
@@ -32,17 +32,9 @@
     
     <div class="row content-row">
     
-          <article class="frontpage-block-1">
+          <article class="frontpage-block-1 clearfix">
 
-            <div class="large-5 medium-5 columns">
-
-              <div class="flex-video widescreen vimeo">
-                <!-- <iframe src="http://player.vimeo.com/video/136114837" width="400" height="225" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> -->
-                <iframe src="//player.vimeo.com/video/39822385?title=0&amp;byline=0&amp;portrait=0" style="width: 100%; height: auto; border: 0;" allowfullscreen></iframe>
-              </div>
-
-            </div>
-            <div class="frontpage-content-1 large-7 medium-7 columns">
+            <div class="frontpage-content-1 large-7 large-push-5 medium-12 columns">
               
               <h3><a href="#" title="">Article Title</a></h3>
               
@@ -54,6 +46,16 @@
                 cillum dolore eu fugiat nulla pariatur. 
               </p>
             </div>
+
+            <div class="large-5 large-pull-7 medium-12 columns">
+
+              <div class="flex-video widescreen vimeo">
+                <!-- <iframe src="http://player.vimeo.com/video/136114837" width="400" height="225" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> -->
+                <iframe src="//player.vimeo.com/video/39822385?title=0&amp;byline=0&amp;portrait=0" style="width: 100%; height: auto; border: 0;" allowfullscreen></iframe>
+              </div>
+
+            </div>
+            
             <!-- <a href="#" class="button right" title="">Read More</a> -->
 
           </article>
@@ -70,7 +72,7 @@
           <article class="frontpage-block-2 clearfix">
 
 
-            <div class="frontpage-content-2 large-7 medium-7 columns">
+            <div class="frontpage-content-2 large-7 medium-12 columns">
               <h3><a href="#" title="">Article Title</a></h3>
               <p>
                 <small>
@@ -89,7 +91,7 @@
 
             </div>
 
-             <div class="large-5 medium-5 columns">
+             <div class="large-5 medium-12 columns">
 
               <div class="flex-video widescreen vimeo">
                 <!-- <iframe src="http://player.vimeo.com/video/136114837" width="400" height="225" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> -->
@@ -113,13 +115,11 @@
     
       <div class="row content-row">
     
-          <article class="frontpage-block clearfix">
+          <ul class="frontpage-block-3 medium-block-grid-2 large-block-grid-2">
 
-            <div class="large-5 medium-5 columns">
-              <a class="" href="#" title=""><img class="img-responsive" src="images/blog-img-1.jpeg" alt=""></a>
-              <!-- <a class="" href="#" title=""><img class="img-responsive" src="http://lorempixel.com/640/640/abstract/6" alt=""></a> -->
-            </div>
-            <div class="frontpage-content large-7 medium-7 columns">
+            
+            <li class="frontpage-content-3 ">
+
               <h3><a href="#" title="">Article Title</a></h3>
               <p>
                 <small>
@@ -134,50 +134,141 @@
                 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
                 cillum dolore eu fugiat nulla pariatur. 
               </p>
-            </div>
-            <a href="#" class="button right" title="">Read More</a>
+              <a href="#" class="button right" title="">Read More</a>
 
-          </article>
+            </li>
+            <li class="frontpage-content-3 ">
+            
+              <h3><a href="#" title="">Article Title</a></h3>
+              <p>
+                <small>
+                  Posted on <em><a href="#" title="">2 June 2015</a></em> by <a href="#" title="">Da Moose</a> in
+                  <a href="#" title="">Cars</a>, <a href="#" title="">Sports Cars</a>
+                </small></p>
+              <p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. 
+              </p>
+              <a href="#" class="button right" title="">Read More</a>
+
+            </li>
+            <li class="frontpage-content-3 ">
+            
+              <h3><a href="#" title="">Article Title</a></h3>
+              <p>
+                <small>
+                  Posted on <em><a href="#" title="">2 June 2015</a></em> by <a href="#" title="">Da Moose</a> in
+                  <a href="#" title="">Cars</a>, <a href="#" title="">Sports Cars</a>
+                </small></p>
+              <p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. 
+              </p>
+              <a href="#" class="button right" title="">Read More</a>
+
+            </li>
+            <li class="frontpage-content-3 ">
+            
+              <h3><a href="#" title="">Article Title</a></h3>
+              <p>
+                <small>
+                  Posted on <em><a href="#" title="">2 June 2015</a></em> by <a href="#" title="">Da Moose</a> in
+                  <a href="#" title="">Cars</a>, <a href="#" title="">Sports Cars</a>
+                </small></p>
+              <p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. 
+              </p>
+              <a href="#" class="button right" title="">Read More</a>
+
+            </li>
+
+          </ul>
+
 
         
       </div> <!-- CONTENT ROW -->
 
   </div> <!-- FRONTPAGE-BACKGROUND-BLOCK-3 -->
 
-  <div class="frontpage-background-block-2">
+  <div class="frontpage-background-block-4">
     
     <div class="row content-row">
     
-          <article class="frontpage-block clearfix">
+          <ul class="frontpage-block-4 small-block-grid-1 medium-block-grid-2 large-block-grid-4">
 
-            <div class="large-5 medium-5 columns">
-              <!-- <a class="" href="#" title=""><img class="img-responsive" src="images/blog-img-1.jpeg" alt=""></a> -->
-              <!-- <a class="" href="#" title=""><img class="img-responsive" src="http://lorempixel.com/640/640/abstract/6" alt=""></a> -->
-            </div>
-            <div class="frontpage-content large-7 medium-7 columns">
-              <h3><a href="#" title="">Article Title</a></h3>
+            <li class="frontpage-content-4 large-7 medium-12 columns">
+              <p class="text-center">
+                <img class="th" src="http://lorempixel.com/100/100/people/1" alt="">
               <p>
-                <small>
-                  Posted on <em><a href="#" title="">2 June 2015</a></em> by <a href="#" title="">Da Moose</a> in
-                  <a href="#" title="">Cars</a>, <a href="#" title="">Sports Cars</a>
-                </small></p>
-              <p>
-              <p>
+              <blockquote>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. 
-              </p>
-            </div>
-            <a href="#" class="button right" title="">Read More</a>
+                tempor incididunt ut labore et dolore magna aliqua. 
+                <br>
+                <br>
+                <cite>CEO HTMLfiveDev.com </cite>
+              </blockquote>
 
-          </article>
+            </li>
+            <li class="frontpage-content-4 large-7 medium-12 columns">
+              <p class="text-center">
+                <img class="th" src="http://lorempixel.com/100/100/people/7" alt="">
+              <p>
+              <blockquote>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. 
+                <br>
+                <br>
+                <cite>CEO HTMLfiveDev.com </cite>
+              </blockquote>
+
+            </li>
+            <li class="frontpage-content-4 large-7 medium-12 columns">
+              <p class="text-center">
+                <img class="th" src="http://lorempixel.com/100/100/people/8" alt="">
+              <p>
+              <blockquote>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. 
+                <br>
+                <br>
+                <cite>CEO HTMLfiveDev.com </cite>
+              </blockquote>
+
+            </li>
+            <li class="frontpage-content-4 large-7 medium-12 columns">
+              <p class="text-center">
+                <img class="th" src="http://lorempixel.com/100/100/people/9" alt="">
+              <p>
+              <blockquote>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. 
+                <br>
+                <br>
+                <cite>CEO HTMLfiveDev.com </cite>
+              </blockquote>
+
+            </li>
+            
+          </ul>
 
         
     </div> <!-- CONTENT ROW -->
 
   </div> <!-- FRONTPAGE-BACKGROUND-BLOCK-2 -->
+
 
   <div class="frontpage-background-block-1">
     
